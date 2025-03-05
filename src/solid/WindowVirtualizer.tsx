5033d911b15498a474260de698507959b3b78e34
Denis Semenenko <denis.semenenko@avochato.com>
2025-03-05 18:06:46 -0500

    feat: allow itemSize as a function

--- src/solid/WindowVirtualizer.tsx
+++ src/solid/WindowVirtualizer.tsx
@@ -21,6 +21,7 @@ import {
   createWindowScroller,
   ItemsRange,
   ScrollToIndexOpts,
+  ItemSize,
 } from "../core";
 import { ListItem } from "./ListItem";
 import { RangedFor } from "./RangedFor";
@@ -73,7 +74,7 @@ export interface WindowVirtualizerProps<T> {
    * - If not set, initial item sizes will be automatically estimated from measured sizes. This is recommended for most cases.
    * - If set, you can opt out estimation and use the value as initial item size.
    */
-  itemSize?: number;
+  itemSize?: ItemSize;
   /**
    * While true is set, scroll position will be maintained from the end not usual start when items are added to/removed from start. It's recommended to set false if you add to/remove from mid/end of the list because it can cause unexpected behavior. This prop is useful for reverse infinite scrolling.
    */
